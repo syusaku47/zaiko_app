a9fce09c832e338a14712563d5d9e9c7eb881a05
syusaku <carryppp.gmail.com>
2020-06-26 17:16:28 +0900

    seven

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
   
   root 'home#top'
   get "home/howto" => "home#howto"
+  get "home/about" => "home#about"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
