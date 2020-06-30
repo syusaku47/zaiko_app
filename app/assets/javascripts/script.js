02144412fda21aea00338f25b9c3cd18e5122ae4
syusaku <carryppp.gmail.com>
2020-07-01 08:47:15 +0900

    nine

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
     $(".flash").fadeOut(3000);
     $(".my-photo").fadeIn(1500);
+
+//-----------------モーダル-----------
     $(".menu-icon").click(function(){
         if($('#menu-modal').hasClass('off')){
             $('#menu-modal').removeClass('off');
-            $('#menu-modal').animate({'marginRight':'20%'},300).addClass('on');
+            $('#menu-modal').animate({'marginRight':'40%'},300).addClass('on');
         }else{
             $('#menu-modal').addClass('off');
             $('#menu-modal').animate({'marginRight':'0px'},300);
@@ -18,5 +20,12 @@ $(document).ready(function() {
     var h = $(window).height();
     $('.menu-list').css("height",h);
     }
+
+    $(".singleinbox").click(function(){
+        if ($('#menu-modal').hasClass('on')){
+        $('#menu-modal').addClass('off');
+        $('#menu-modal').animate({'marginRight':'0px'},300);
+    }
+    })
 
     });
